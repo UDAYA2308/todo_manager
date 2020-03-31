c81249c76ffa9aa8d5047e740a21a36aa65bde77
udaya2308 <udaya14apr2000@gmail.com>
2020-03-31 14:43:52 +0530

    add route to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+  post "users", to: "users#login"
   resources :users
-  post "users", to:"users#login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :todos
-
 end
-  
