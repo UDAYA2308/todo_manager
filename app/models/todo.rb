cb5b326e12cce64239a6b525527b5c3367281bb9
udaya2308 <udaya14apr2000@gmail.com>
2020-04-07 12:22:12 +0530

    updated

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,57 @@ class Todo < ActiveRecord::Base
     is_completed = completed ? "[X]" : "[ ]"
     "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
   end
+
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
+
+  def to_displayable_string
+    display_status = completed ? "[X]" : "[ ]"
+    display_date = due_today? ? nil : due_date
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
+  end
+
+  def self.overdue
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date  > ?", Date.today)
+  end
+
+  def self.show_list
+    puts "\nMy Todo-list"
+    puts "\n\n"
+    puts "Overdue"
+    puts overdue.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+    puts "Due Today"
+    puts due_today.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+    puts "Due Later"
+    puts due_later.map { |todo| todo.to_displayable_string }
+    puts "\n"
+  end
+
+  def self.add_task(task)
+    todo_text = :todo_text
+    due_in_days = :due_in_days
+    create!(todo_text: task[todo_text], due_date: Date.today + task[due_in_days], completed: false)
+  end
+
+  def self.mark_as_complete!(id)
+    todo = find(id)
+    todo.completed = true
+    todo.save
+    todo
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_052025) do
+ActiveRecord::Schema.define(version: 2020_04_07_064529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
